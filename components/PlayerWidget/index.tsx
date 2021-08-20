73dc86852f6b81300044bc43dde4a2aa8eb07e69
Walker Lillard <66097567+astrodesign@users.noreply.github.com>
2021-08-20 10:23:01 -0500

    fixed up the Progress bar on the Play Widget
    on expo-av, you're supposed to use "playableDurationMillis" instead of "duratoinMillis" as a variable when calculating playtime percentage. now, the progress bar tracks the current song progress based on the duration of the song.

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,124 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import Colors from '../../components'; 
-import {AntDesign, FontAwesome } from '@expo/vector-icons'; 
-import {Audio} from 'expo-av'; 
-//import Song from '../../types'; 
+import React, {useContext, useEffect, useState} from 'react';
+import {Text, Image, View, TouchableOpacity} from 'react-native';
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { API, graphqlOperation } from 'aws-amplify';
 
-const song ={
-    id: '4',
-    imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-    title: 'Stay At Home Astronaut',
-    artist: 'Walker Lillard',
-    uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3'
-  }
+import styles from './styles';
+import {Song} from "../../types";
+import {Sound} from "expo-av/build/Audio/Sound";
+
+//import { AppContext } from '../../AppContext';
+//import {getSong} from "../../src/graphql/queries"; 
+
+
+
 
 const PlayerWidget = () => {
+    
 
-    const [sound, setSound] = useState<Sound|null>(null); 
-    const [isPlaying, setIsPlaying]= useState<boolean>(true); 
+    const song ={
+        id: '4',
+        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        title: 'Stay At Home Astronaut',
+        artist: 'Walker Lillard',
+        uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3'
+      }
+    
 
-    const onPlaybackStatusUpdate = (status) => {
-        setIsPlaying(status.isPlaying); 
-    }
+      const [sound, setSound] = useState<Sound|null>(null);
+      const [isPlaying, setIsPlaying] = useState<boolean>(true);
+      const [duration, setDuration] = useState<number|null>(null);
+      const [position, setPosition] = useState<number|null>(null);
 
-    const playCurrentSong = async () =>{
-        //unloads the song that is already playing before playing new song
-        if (sound){
-            await sound.unloadAsync(); 
+      const onPlaybackStatusUpdate = (status) => {
+        setIsPlaying(status.isPlaying);
+        setDuration(status.playableDurationMillis);
+        setPosition(status.positionMillis);
+      }
+    
+
+      const playCurrentSong = async () => {
+        if (sound) {
+          await sound.unloadAsync();
         }
-
-        const { sound: newSound } = await Audio.Sound.createAsync(
-            {uri: song.uri}, 
-            { shouldPlay: isPlaying}, 
-            onPlaybackStatusUpdate
+    
+        const { sound: newSound } = await Sound.createAsync(
+          { uri: song.uri },
+          { shouldPlay: isPlaying },
+          onPlaybackStatusUpdate
         )
+    
         setSound(newSound)
-    }
+      }
 
     useEffect(()=> {
-        //play the song
         playCurrentSong(); 
     }, [])
 
     const onPlayPausePress = async () => {
-        if(!sound){
-            return; 
+        if (!sound) {
+          return;
         }
-        if (isPlaying){
-            await sound.stopAsync()
+        if (isPlaying) {
+          await sound.stopAsync();
+        } else {
+          await sound.playAsync();
         }
-        else {
-            await sound.playAsync(); 
+      }
+
+      const getProgress = () => {
+        if (sound === null || duration === null || position === null) {
+          return 0;
         }
-    }
+    
+        return (position/duration * 100); 
+      }
+      console.log(position)
+    
+      if (!song) {
+        return null;
+      }
 
     return (
         <View style={styles.container}>
-            <Image source={{uri: song.imageUri}} style={styles.image}/>
-        
-        <View style={styles.rightContainer}>    
-            <Text style={styles.title}>{song.title}</Text>
-            <Text style={styles.artist}>{song.artist}</Text>
-        </View>
-        <View style={styles.buttonContainer}>
-            <AntDesign name='hearto' size={25} color={'white'}/>
-            
-            
-            <TouchableOpacity onPress={onPlayPausePress}>
-                <FontAwesome name={isPlaying ? 'pause' : 'play'} size={25} color={'white'}/>
-            </TouchableOpacity>
-
-        </View>
-        </View>
-    )
-}
-
-export default PlayerWidget
-
-const styles = StyleSheet.create({
-    image:{
-        width: 75, 
-        height: 75, 
-        borderRadius: 6, 
-        marginRight: 10, 
-    }, 
-
-    title:{
-        color: 'white', 
-        fontSize: 18,
-        marginVertical: 10, 
-        fontWeight: 'bold'
-    }, 
-
-    artist:{
-        color: 'lightgray', 
-        fontSize: 18, 
-        marginLeft: 20
-    }, 
-
-    container:{
-        position: 'absolute', 
-        bottom: 77, 
-        flexDirection: 'row', 
-        backgroundColor: '#131313', 
-        width: '100%', 
-        borderWidth: 1, 
-        borderColor: 'black', 
-        alignItems: 'center'
-    }, 
-
-    rightContainer:{
-        flex: 1, 
-        flexDirection: 'row', 
-        alignItems: 'center'
-    }, 
+          <View style={[styles.progress, { width: `${getProgress()}%`}]} />
+          <View style={styles.row}>
+            <Image source={{ uri: song.imageUri }} style={styles.image} />
+            <View style={styles.rightContainer}>
+              <View style={styles.nameContainer}>
+                <Text style={styles.title}>{song.title}</Text>
+                <Text style={styles.artist}>{song.artist}</Text>
+              </View>
     
-    buttonContainer:{
-        alignItems: 'center', 
-        flexDirection: 'row', 
-        justifyContent: 'space-around', 
-        width: 100, 
+              <View style={styles.buttonContainer}>
+                <AntDesign name="hearto" size={30} color={"white"}/>
+                <TouchableOpacity onPress={onPlayPausePress}>
+                  <FontAwesome name={isPlaying ? 'pause' : 'play'} size={30} color={"white"}/>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      )
     }
-
-})
+    
+    export default PlayerWidget;
---
+++ components/PlayerWidget/styles.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet} from "react-native";
+
+
+const styles = StyleSheet.create({
+    image:{
+        width: 75, 
+        height: 75, 
+        borderRadius: 6, 
+        marginRight: 10, 
+    }, 
+    
+
+    title:{
+        color: 'white', 
+        fontSize: 12,
+        marginVertical: 10, 
+        fontWeight: 'bold'
+    }, 
+
+    artist:{
+        color: 'lightgray', 
+        fontSize: 12, 
+        marginLeft: 20
+    }, 
+
+    container:{
+        position: 'absolute', 
+        bottom: 77, 
+        flexDirection: 'column', 
+        backgroundColor: '#131313', 
+        width: '100%', 
+        borderWidth: 1, 
+        borderColor: 'black', 
+        
+    }, 
+
+    rightContainer:{
+        flex: 1, 
+        flexDirection: 'row', 
+        alignItems: 'center',  
+       }, 
+    
+    buttonContainer:{
+        alignItems: 'center', 
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        width: 100, 
+        left: 100
+    }, 
+
+    row:{
+        flexDirection: 'row', 
+        justifyContent: 'space-around'
+
+    }, 
+
+    progress:{
+        height: 4, 
+        backgroundColor: 'white', 
+    }
+
+})
+
+export default styles;
